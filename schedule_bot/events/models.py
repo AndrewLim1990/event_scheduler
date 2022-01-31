acd514c6337b8357ece4438135e6c0eb57f5293f
AndrewLim1990 <andrewlim90@gmail.com>
2022-01-31 08:23:44 -0800

    Adds event models

--- schedule_bot/events/models.py
+++ schedule_bot/events/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Event(models.Model):
+    """
+    Represents a specific event
+    """
+    name = models.CharField(max_length=512, null=True, default=None)
+    # On delete, must choose new host:
+    host_member = models.ForeignKey(User, on_delete=models.PROTECT, related_name="host_member")
+    members = models.ManyToManyField(User, null=True, default=None)
+
+
+class EventTime(models.Model):
+    """
+    Represents a proposed event time
+    """
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    date_time_start = models.DateTimeField()
+    date_time_end = models.DateTimeField()
