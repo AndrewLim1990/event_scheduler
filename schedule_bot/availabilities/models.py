ea5d6f1d425a278749dd6bba53a52f5e91a1ba4a
AndrewLim1990 <andrewlim90@gmail.com>
2022-01-31 09:20:29 -0800

    Adds dummy method to check availabilities

--- schedule_bot/availabilities/models.py
+++ schedule_bot/availabilities/models.py
@@ -7,10 +7,28 @@ class Availability(models.Model):
     category = models.CharField(default=None, null=True, max_length=512)
     is_available = models.BooleanField()
 
+    def check_availability(self, suggested_date):
+        """
+        Checks availability against suggested date
+
+        :param suggested_date:
+        :return:
+        """
+        raise NotImplemented
+
 
 class DateAvailability(Availability):
     date_time_start = models.DateTimeField()
     date_time_end = models.DateTimeField()
+
+    def check_availability(self, suggested_date):
+        """
+        Checks availability against suggested date
+
+        :param suggested_date:
+        :return:
+        """
+        return suggested_date
 
 
 class RepeatedDateAvailability(DateAvailability):
