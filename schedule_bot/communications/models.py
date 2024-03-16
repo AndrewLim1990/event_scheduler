8bed90bc653f9b3ecc2415deffbc5b4218f1e318
Andrew Lim <andrew.lim@teamblind.com>
2024-03-16 11:07:20 -0700

    Adds unique phone number constraint

--- schedule_bot/communications/models.py
+++ schedule_bot/communications/models.py
@@ -5,7 +5,7 @@ from events.models import Event
 
 class UserContactInfo(models.Model):
     user = models.OneToOneField(Member, on_delete=models.CASCADE, related_name="user_contact_info")
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, unique=True)
 
 
 # Create your models here.
